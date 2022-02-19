d1ac81474f7a38313a0b02d44627e5d41a1073ab
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-20 00:36:54 +0530

    learnt how to declear ref. variable and how to initialized it

--- src/com/company/day1/Main.java
+++ src/com/company/day1/Main.java
@@ -3,8 +3,13 @@ package com.company.day1;
 public class Main {
     public static void main(String[] args) {
 
+        Student s; // declaring ref. variable s of datatype Student. ( NOT Initializing )
+        //  System.out.println(s);  // gives error bcz ref. variable has not been Initialized.
+        s = new Student();  // initializing the ref. variable.
+        System.out.println(s);  // prints the location and type of ref. variable s is having.
+
         // creating a obj of Student class.
-        Student s1 = new Student();
+        Student s1 = new Student();  // declared and initialized ref var s1 of datatype/class Student.
 
         // set the attribute/property's value for s1 obj.
         s1.name = "Amanjeet";

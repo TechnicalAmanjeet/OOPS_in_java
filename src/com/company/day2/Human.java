4e2946fd783fdca8f26383ee898e1699fec9a248
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-21 02:53:15 +0530

    learnt about static method

--- src/com/company/day2/Human.java
+++ src/com/company/day2/Human.java
@@ -4,11 +4,15 @@ public class Human {
     String fullName, gender;
     int age, salary;
     boolean married;
+    // creating static variable.
     static long population = 0; // keep track of human object.
 
     Human(){
-        population += 1;
+//        this.population += 1;
+        Human.population += 1; // it's a good convention that always try to access static variable and method by
+        // using Class name. ( in this case it's Human ).
     }
+
 
     public Human(String fullName, String gender, int age, int salary, boolean married) {
         this();
@@ -19,5 +23,9 @@ public class Human {
         this.married = married;
     }
 
+    // creating static methods.
+    static void staticMethod(){
+        System.out.println("messaging through static method of Human class.");
+    }
 
 }
--- src/com/company/day2/Main.java
+++ src/com/company/day2/Main.java
@@ -13,9 +13,13 @@ public class Main {
 
         // creating one more object
         Human h2 = new Human();
-        System.out.println("After creating 2nd human object : ");
+        System.out.println("After creating 2nd   human object : ");
         System.out.println("From Human classname : " + Human.population);
         System.out.println("from h1 object : " + Human.population);
+
+
+        // calling static method
+        Human.staticMethod();
 
     }
 }

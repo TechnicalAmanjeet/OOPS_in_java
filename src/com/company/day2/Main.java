78a0e855af1f91a23f6809c209a4ed98b6765d82
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-21 03:25:39 +0530

    resolved issue that how to call a staic method/ properties from a non static method

--- src/com/company/day2/Main.java
+++ src/com/company/day2/Main.java
@@ -27,18 +27,33 @@ public class Main {
         Human.staticMethod();
         System.out.println(Human.population); // Human.population;
 
-        // we can not access non-static method from a static method.
-//        greeting(); // error bcz we can not access non-static methods.
-        greeting1(); // will not give error.
+//        // we can not access non-static method from a static method.
+////        greeting(); // error bcz we can not access non-static methods.
+//        greeting1(); // will not give error.
+
+
+        // we can call a non-static method from static method by this way.
+        Main obj = new Main();
+        obj.greeting(); // now it will not give an error.
+
     }
 
     // will give error when we call it from a static method. bcz its non-static method.
     void greeting(){
         System.out.println("greet");
+//        greeting1(); // will not give error bcz we are calling a non-static method from a static one.
     }
 
-    // will not give any error if we call either from static or non static method.
+
+
+    // will not give any error if we call either from static or non-static method.
     static void greeting1(){
         System.out.println("greet from static method.");
+        // greeting(); // will give error bcz we are calling a static method from a non-static method.
+
+        // but there is a way to remove above error => first create an instance of that class here
+        // and then call that method from that instance.
+        Main obj = new Main();
+        obj.greeting(); // will not give error.
     }
 }

10498e7dd7058ed6881c297b0b5959997a29bb91
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-27 12:03:29 +0530

    Done with 4th video of oops

--- src/com/company/day4/Main.java
+++ src/com/company/day4/Main.java
@@ -3,5 +3,6 @@ package com.company.day4;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Starting with oops 4 video. will learn about access control.");
+        System.out.println("done with 4th video of oops.");
     }
 }

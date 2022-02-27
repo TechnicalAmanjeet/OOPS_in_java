8441a21847c5bd241e60727942b1c927a6d5285d
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-27 09:42:29 +0530

    starting with 4th video:

---
+++ src/com/company/day4/Main.java
@@ -0,0 +1,7 @@
+package com.company.day4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting with oops 4 video. will learn about access control.");
+    }
+}

41faad7d3b3e849cc826bac91a0efb478067b247
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-02-28 19:41:45 +0530

    starting with 5th video will learn about abstract classes and interfaces

---
+++ src/com/company/day5/Main.java
@@ -0,0 +1,7 @@
+package com.company.day5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting with fourth video. will learn about abstract classes and interfaces in python.");
+    }
+}
